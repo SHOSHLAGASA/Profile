c58a23953a4a3c9d1d1fc0090f90a06dcc3598bc
shoshana lagasa <shoshanalagasa850@gmail.com>
2022-02-28 14:43:25 -0600

    adding information

--- components/views/Resume.js
+++ components/views/Resume.js
@@ -1,5 +1,79 @@
 import html from "html-literal";
 
 export default st => html`
-  <div class="Box"></div>
+  <div class="Box">
+    <h3>Professional History</h3>
+    <h4>Assistant Teacher 2021 – Present Torah Prep School</h4>
+    <ul>
+      <li>Helps with a variety of tasks related to the pre-school.</li>
+    </ul>
+    <h4>Owner/Caretaker 2018 – 2021 In Home Play Group - Saint Louis, MO</h4>
+    <ul>
+      <li>Coordinated care of children daily</li>
+      <li>
+        Tracked children's behavioral, sleeping and eating patterns and
+        communicated daily with parent
+      </li>
+      <li>
+        Ensured and maintained sanitary and safety standards including site
+        cleaning, sanitizing toys and performing safety checks
+      </li>
+      <li>
+        Advised and coached parents on strategies to encourage healthy child
+        development
+      </li>
+    </ul>
+    <h4>Clerk 2013 – 2014</h4>
+    <h5>Pazz- Telaviv, Israel</h5>
+    <ul>
+      <li>Maintained store in neat and organized fashion</li>
+      <li>
+        Observed cars at gas pumps and ensured theft attempts were documented
+      </li>
+      <li>Upsold customers on drinks and snack items</li>
+      <li>Trained new clerks on proper procedures</li>
+      <li>Consistently exceeded sales goals</li>
+      <li>Was considered top employee</li>
+    </ul>
+    <h4>Barista 2012 – 2013</h4>
+    <h5>Kakao - Lod, Israel</h5>
+    <ul>
+      <li>
+        Learned every menu preparation and numerous off label drinks to meet all
+        customer needs
+      </li>
+      <li>
+        Designed and stocked display cases with attractive arrangements to
+        promote special food items
+      </li>
+      <li>Managed site cleaning and sanitation of food prep areas</li>
+      <li>Maintained secure and accurate cash drawers</li>
+      <li>
+        Used customer service skills and friendly personality to create and
+        cultivate a regular clientele
+      </li>
+    </ul>
+    <h4>Military Experience</h4>
+    <h5>Prison Warden 2011 – 2013</h5>
+    <h5>IDF (Israeli defense force) - Israel</h5>
+    <ul>
+      <li>
+        Excelled at seeing and reporting out of place detail to prompt immediate
+        inspection of cells and common areas for infractions
+      </li>
+      <li>
+        Cooperatively communicated with inmates and colleagues to facilitate
+        smooth operation of facility
+      </li>
+      <li>
+        Learned and implemented necessary procedures for searching inmates for
+        illegal substances, weapons, cell phones and other contraband.
+      </li>
+      <li>
+        Studied and carried out procedures for transporting prisoners and
+        visitors inside and outside the prison
+      </li>
+      <li>Performed watchtower guard dut</li>
+    </ul>
+  </div>
 `;
--- components/views/Skills.js
+++ components/views/Skills.js
@@ -9,5 +9,27 @@ export default st => html`
     <p>Axios, Navigo, HTTP RESTful API’s, VS Code, Markdown,</p>
     <p>GitHub, UI/UX, Jira, Agile, OOP, SPA’s,</p>
     <p>CRUD Operations, Deploying to Netlify and Heroku</p>
+    <h3>Education</h3>
+    <p>
+      Savvy Coder Full Stack Web Development Boot Camp – 2021 Agile
+    </p>
+    <p>Certification through ICAgile – 2021</p>
+    <p>
+      Savvy graduates receive a Scrum certification from ICAgile. They have
+      demonstrated their understanding of the Agile/Scrum Methodology by
+      presenting a final Capstone Project that embodies the methodology and
+      project preparation. The students work in Jira, create User Stories,
+      manage tasks and contributions to Sprints in Jira. They work in teams to
+      plan Sprints, hold daily standups to groom the backlog, and participate in
+      weekly Sprint Retrospectives. They work on selected user stories and keep
+      communication flowing to ensure everyone is on the same page for the next
+      increment of work. All project tracking is done in Jira
+    </p>
+    <h3>Capstone Project</h3>
+    <p>
+      L likes to design and sew. My desire is to eventually create my own
+      collections, so l built a website for children’s clothing.
+      <a href="https://shanaa.netlify.app/">capstone Project Web Address</a>
+    </p>
   </div>
 `;
